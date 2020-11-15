695b3d5d6a7888ca0c8713f3315ede4edffb8ea4
Rashid <rashid.tajdeen@smail.inf.h-brs.de>
2020-11-15 12:11:51 +0530

    created functions 5 to 8 in list_manager.py

--- list_manager.py
+++ list_manager.py
@@ -18,17 +18,31 @@ def reverse_order(list):
 
 def stringfy_list(list):
     '''returns a list with all elements turned into strings'''
+    string_list = [str(i) for i in list]
+    return string_list
     pass
 
 def multiply_list(list, multiple):
     '''returns the list with all elements multipled by the value multiple'''
+    mult_list = [multiple*i for i in list]
+    return mult_list
     pass
 
 def get_highest_value(list):
     '''returns the highest value of the list'''
+    max_value = 0
+    for i in list:
+        if i>max_value:
+            max_value=i
+    return max_value
     pass
 
 def get_lowest_value(list):
     '''returns the lowest value of the list'''
+    min_value = list[0]
+    for i in list[1:]:
+        if i<min_value:
+            min_value=i
+    return min_value
     pass
 
